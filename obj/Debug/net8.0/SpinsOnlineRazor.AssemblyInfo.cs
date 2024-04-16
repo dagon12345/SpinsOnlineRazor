b849468023de187444cf792456243cdda0165f3a
dagon12345 <lanceespina478@gmail.com>
2024-04-16 20:51:37 +0800

    Done with the button each row

--- obj/Debug/net8.0/SpinsOnlineRazor.AssemblyInfo.cs
+++ obj/Debug/net8.0/SpinsOnlineRazor.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SpinsOnlineRazor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e9a74b42d1d0308f9e7cd3453537861fe06eaaeb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ad6c658531810386e5946b26295ecc301f9eb26d")]
 [assembly: System.Reflection.AssemblyProductAttribute("SpinsOnlineRazor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SpinsOnlineRazor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
