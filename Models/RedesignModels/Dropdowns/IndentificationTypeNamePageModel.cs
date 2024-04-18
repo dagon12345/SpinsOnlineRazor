eda6b7a780d7a4353cc73478ccc468bd40e9ee00
dagon12345 <lanceespina478@gmail.com>
2024-04-18 22:10:07 +0800

    Working on Id dropdown on create

---
+++ Models/RedesignModels/Dropdowns/IndentificationTypeNamePageModel.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using SpinsOnlineRazor.Data;
+
+namespace SpinsOnlineRazor.Models.RedesignModels.Dropdowns
+{
+    /*The preceding code creates a SelectList to contain the list of department names. 
+    If selectedDepartment is specified, that department is selected in the SelectList.*/
+    public class IndentificationTypeNamePageModel
+    {
+        public SelectList IdentificationNameSL { get; set; }
+        public void PopulateIndetificationDropDownList(SpinsContext _context, object selectedId = null)
+        {
+            var identificationsQuery = from d in _context.IdentificationTypes
+                                       orderby d.Name // Sort by Name
+                                       select d;
+
+            IdentificationNameSL = new SelectList(identificationsQuery.AsNoTracking(),
+                nameof(IdentificationType.IdentificationTypeID),
+                nameof(IdentificationType.Name),
+                selectedId);
+        }
+    }
+}
